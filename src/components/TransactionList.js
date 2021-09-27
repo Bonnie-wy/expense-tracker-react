47fe660e21bbf9878cb27ccdfe3327da2da0ac78
Bonnie <bonnie.wong@and.digital>
2021-09-27 23:14:49 +0100

    added components and context

---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import { Transaction } from './Transaction';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+            </ul>
+        </>
+    )
+}
